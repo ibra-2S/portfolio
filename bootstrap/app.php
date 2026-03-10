1bb3b31f5a2ee82645d493a3448fd6f35e1a4569
Ibrahima Sory Soumah <ibrahimsmh01@gmail.com>
2026-03-10 15:15:55 +0000

    Fix HTTPS - trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo(fn () => route('admin.login'));
+        $middleware->trustProxies(at: '*');
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
